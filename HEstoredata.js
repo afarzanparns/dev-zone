c7a07a2cf7c4525b92484bf2ff5a15fdf3e84afe
afarzanparns <120696175+afarzanparns@users.noreply.github.com>
2024-12-22 18:12:44 +0000

    Add files via upload

---
+++ HEstoredata.js
@@ -0,0 +1,219 @@
+
+// images courtesy of pexels.com and unsplash.com
+// colour courtesy of https://coolors.co
+
+
+let shopItemsData = [{
+    id: 'eyspoiu',
+  name: 'Himalayan salt',
+ price: 4,
+  desc: 'Himalayan pink salt',
+   img: 'img-1.jpg',
+},{
+    id: 'swerver',
+  name: 'Shilagit 10g',
+ price: 22,
+  desc: 'shilagit 10g',
+   img: 'img-2.jpg',
+},{
+    id: 'shoikgu',
+  name: 'Incense burner',
+ price: 15,
+  desc: 'pretty burner',
+   img: 'img-3.jpg',
+},{
+    id: 'swedish',
+  name: 'Swedish log fire',
+ price: 9,
+  desc: 'Swedish fire log',
+   img: 'img-4.jpg',
+} ,{
+    id: 'toop7mi',
+  name: 'Toothpick',
+ price: 0.4,
+  desc: 'toothpick pack / cocktail sticks',
+   img: 'img-5.jpg',
+}, {
+    id: 'mishcon',
+  name: 'Miswak brush',
+ price: 1,
+  desc: 'the first natural toothbrush',
+   img: 'img-6.jpg',
+}, {
+    id: 'iqeojaw',
+  name: 'Essential oils',
+ price: 8,
+  desc: 'essential oil six pack',
+   img: 'img-7.jpg',
+}, {
+    id: 'visions',
+  name: 'Antimony',
+ price: 5,
+  desc: 'antimony - kohl - surma - natural eyeliner',
+   img: 'img-8.jpg',
+}, {
+    id: 'abensis',
+  name: 'Hard skin tool (white)',
+ price: 3,
+  desc: 'callous remover in white',
+   img: 'img-9.jpg',
+}, {
+    id: 'xylitol',
+  name: 'Hard skin tool (pink)',
+ price: 3,
+  desc: 'callous remover in pink',
+   img: 'img-10.jpg',
+}, {
+    id: 'bientot',
+  name: 'Avon ceramic massager',
+ price: 3,
+  desc: 'Avon calling!',
+   img: 'img-11.jpg',
+}, {
+    id: 'torsion',
+  name: 'Camel hump oil',
+ price: 20,
+  desc: 'oil of camel hump',
+   img: 'img-12.jpg',
+}, {
+    id: 'rameses',
+  name: 'Argan oil',
+ price: 10,
+  desc: 'Argan oil',
+   img: 'img-13.jpg',
+}, {
+    id: 'genteel',
+  name: 'Rapeseed oil',
+ price: 8,
+  desc: 'rapeseed oil',
+   img: 'img-14.jpg',
+}, {
+    id: 'impulse',
+  name: 'Plate, eight-leaf pattern',
+ price: 20,
+  desc: 'heavy serving plate',
+   img: 'img-15.jpg',
+}, {
+    id: 'happier',
+  name: 'Plate, four-leaf pattern',
+ price: 20,
+  desc: 'heavy serving plate',
+   img: 'img-16.jpg',
+}, {
+    id: 'lattice',
+  name: 'Plate, lattice pattern',
+ price: 20,
+  desc: 'heavy serving plate',
+   img: 'img-17.jpg',
+}, {
+    id: 'sparkle',
+  name: 'Yoga mat - pink',
+ price: 7,
+  desc: 'pink yoga mat',
+   img: 'img-18.jpg',
+}, {
+    id: 'theuthe',
+  name: 'Yoga mat - green',
+ price: 7,
+  desc: 'green yoga mat',
+   img: 'img-19.jpg',
+}, {
+    id: 'rockies',
+  name: 'Yoga mat - blue',
+ price: 7,
+  desc: 'blue yoga mat',
+   img: 'img-20.jpg',
+}, {
+    id: 'yardley',
+  name: 'Yoga mat - purple',
+ price: 7,
+  desc: 'purple yoga mat',
+   img: 'img-21.jpg',
+}, {
+    id: 'schwarz',
+  name: 'Yoga mat - black',
+ price: 7,
+  desc: 'black yoga mat',
+   img: 'img-22.jpg',
+}, {
+    id: 'mellifl',
+  name: 'Royal forest honey',
+ price: 0,
+  desc: 'royal forest honey',
+   img: 'img-23.jpg',
+}, {
+    id: 'fractal',
+  name: 'Mulberry',
+ price: 3,
+  desc: 'dry mulberry',
+   img: 'img-24.jpg',
+}, {
+    id: 'tibetan',
+  name: 'Sidr',
+ price: 0,
+  desc: 'sidr',
+   img: 'img-25.jpg',
+}, {
+    id: 'quantum',
+  name: 'Spice tea',
+ price: 1,
+  desc: 'made from six spices',
+   img: 'img-26.jpg',
+}, {
+    id: 'zoology',
+  name: 'Care community mat',
+ price: 0.05,
+  desc: 'circular mat',
+   img: 'img-27.jpg',
+}, {
+    id: 'vinegar',
+  name: 'Organic apple cider vinegar',
+ price: 0,
+  desc: 'organic product',
+   img: 'img-28.jpg',
+}, {
+    id: 'dextere',
+  name: 'Saffron',
+ price: 0,
+  desc: 'posh spice',
+   img: 'img-29.jpg',
+}, {
+    id: 'incence',
+  name: 'incense pack',
+ price: 0,
+  desc: 'incense pack',
+   img: 'img-30.jpg',
+}, {
+    id: 'reasons',
+  name: 'Dry Turkey fig',
+ price: 0,
+  desc: 'dry Turkey fig',
+   img: 'img-31.jpg',
+}, {
+    id: 'healing',
+  name: 'Healing hand',
+ price: 10,
+  desc: 'healing hand',
+   img: 'img-32.jpg',
+}, {
+    id: 'bywater',
+  name: 'Natural spring water',
+ price: 0,
+  desc: 'natural spring water',
+   img: 'img-33.jpg',
+}, {
+    id: 'jamaica',
+  name: 'Royal Yemeni Sidr Honey',
+ price: 0,
+  desc: 'royal Yemeni sidr honey',
+   img: 'img-34.jpg',
+}, {
+    id: 'rubicon',
+  name: 'Gemstones',
+ price: 0,
+  desc: 'gemstones',
+   img: 'img-35.jpg',
+}
+
+
+];
